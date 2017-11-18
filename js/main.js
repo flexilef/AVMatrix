22e3ad2053ab4a5d84e4c9eee20dbeabcfe15508
Felix Lee <felix@Felixs-MacBook-Pro.local>
2017-11-17 23:38:41 -0800

    Saving work: example of creating a table of the data in a csv file

--- js/main.js
+++ js/main.js
@@ -51,3 +51,99 @@ var list = d3.select("#example3")
     .text(function(d) {
             return d;
         });
+
+/*
+Example 4
+ */
+
+var domain_explicit_csv = d3.csv("deldroid-input/domain-explicit-communication-1.csv",
+    function(error, data) {
+        if (error) throw error;
+        console.log(data); // [{"Hello": "world"}, …]
+
+        //csv file is loaded as data: an array of objects and an array (of the column names)
+        drawTableFromCsv(data)
+    });
+
+
+//create a table of one row
+
+function drawTableFromCsv(data) {
+    console.log("COLUMNS:" + data.columns.length);
+    var cols = data.columns;
+
+    var tableHeader = d3.select("#example4")
+        .append("table")
+        .attr("id", "domain-table")
+        .selectAll("th")
+        .data(data.columns)
+        .enter()
+        .append("th")
+        .text( function(d) { return d; });
+
+    var tableRows = d3.select("#domain-table")
+        .selectAll("tr")
+        .data(data)
+        .enter()
+        .append("tr");
+
+    //This can be the header... now just have to extract the data
+    var tableData = tableRows.append("td")
+        .text(function(d) { return d['Package']; })
+        .append("td");
+
+    //Table row data
+    //TODO: All of this needs to be looped... or even better use d3 selection mechanisms
+    tableRows.append("td")
+        .text(function(d) { return d['Component']; })
+        .append("td");
+
+    tableRows.append("td")
+        .text(function(d) { return d['ID']; })
+        .append("td");
+
+    tableRows.append("td")
+        .text(function(d) { return d['0']; })
+        .append("td");
+
+    tableRows.append("td")
+        .text(function(d) { return d['1']; })
+        .append("td");
+
+    tableRows.append("td")
+        .text(function(d) { return d['2']; })
+        .append("td");
+
+    tableRows.append("td")
+        .text(function(d) { return d['3']; })
+        .append("td");
+
+    tableRows.append("td")
+        .text(function(d) { return d['4']; })
+        .append("td");
+
+    tableRows.append("td")
+        .text(function(d) { return d['5']; })
+        .append("td");
+}
+
+//
+//
+// var matrix = [
+//     [11975,  5871, 8916, 2868, 1],
+//     [ 1951, 10048, 2060, 6171, 2],
+//     [ 8010, 16145, 8090, 8045, 3],
+//     [ 1013,   990,  940, 6907, 4]
+// ];
+//
+// var tr = d3.select("#example5")
+//     .append("table")
+//     .selectAll("tr")
+//     .data(matrix)
+//     .enter().append("tr");
+//
+// var td = tr.selectAll("td")
+//     .data(function(d) { return d; })
+//     .enter().append("td")
+//     .text(function(d) { return d; });
+//
