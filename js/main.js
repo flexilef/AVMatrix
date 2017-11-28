136529b684fee74ba390ad316d76564fd304a649
Felix Lee <fchanlee@mail.sfsu.edu>
2017-11-27 20:56:44 -0800

    Created new JSON structure - more complicated but should be able to handle analysis results.
    New JSON structure:
    * array of domains (columns)
    * array of packages (side headers - not implemented yet)
    * array of rows (each row is an object encapsulating a component's information, allowing us to pull data we need to display analysis results)
    
    Updated methods with new names (ends with "2") but left old methods intact for reference.

--- js/main.js
+++ js/main.js
@@ -68,7 +68,7 @@ var domain_explicit_csv = d3.csv("deldroid-input/domain-explicit-communication-1
 
 //create a table from a csv file
 function drawTableFromCsv(data, divId, tableId) {
-    console.log("COLUMNS:" + data.columns.length);
+    // console.log("COLUMNS:" + data.columns.length);
     var colHeaders = data.columns;
 
     //This creates the table header
@@ -172,7 +172,7 @@ var permission_file = d3.csv("deldroid-input/domain-permission-granted-1.csv",
                 var groupedData = permission_data.concat(explicit_data);
                 groupedData["columns"] = groupedColumns;
 
-                console.log(groupedData);
+                // console.log(groupedData);
                 drawTableFromCsv(groupedData, "example6", "table-grouped");
             });
     });
@@ -233,13 +233,10 @@ function renderJsonTable(table) {
     var row_data = [];
     table.rows.forEach(function(row) {
         create_table_rows(row);
-        console.log(row);
+        // console.log(row);
     });
 }
 
-/*
-debug
- */
 function print_debug(columns) {
     console.log(columns);
 }
@@ -303,3 +300,282 @@ function create_table_rows(rows) {
 }
 
 renderJsonTable(tableJSON);
+
+/*
+--------------------------------------------------------------------------------------------------
+Example 8 - New JSON
+--------------------------------------------------------------------------------------------------
+ */
+
+var tableJSON2 = {
+    "domains": [
+        {
+            "name": "explicit",
+            "elements": [
+                {
+                    "name": 0
+                },
+                {
+                    "name": 1
+                },
+                {
+                    "name": 2
+                },
+                {
+                    "name": 3
+                },
+                {
+                    "name": 4
+                },
+                {
+                    "name": 5
+                }
+            ]
+        },
+        {
+            "name": "implicit",
+            "elements": [
+                {
+                    "name": 0
+                },
+                {
+                    "name": 1
+                },
+                {
+                    "name": 2
+                },
+                {
+                    "name": 3
+                },
+                {
+                    "name": 4
+                },
+                {
+                    "name": 5
+                }
+            ]
+        },
+        {
+            "name": "permission granted",
+            "elements": [
+                {
+                    "name": "Location",
+                    //"image": "location.png"
+                },
+                {
+                    "name": "SMS",
+                    //"image": "sms.png"
+                },
+                {
+                    "name": "Bluetooth",
+                    //"image": "bluetooth.png"
+                }
+            ]
+        },
+        {
+            "name": "permission usage",
+            "elements": [
+                {
+                    "name": "Location",
+                    //"image": "location.png"
+                },
+                {
+                    "name": "SMS",
+                    //"image": "sms.png"
+                },
+                {
+                    "name": "Bluetooth",
+                    //"image": "bluetooth.png"
+                }
+            ]
+        }
+    ],
+    "packages": [
+        {
+            "name": "Messaging",
+            "components": [
+                {
+                    "name": "ListMsgs",
+                    "target_sdk": "1"
+                },
+                {
+                    "name": "Composer",
+                    "target_sdk": "1"
+                },
+                {
+                    "name": "Sender",
+                    "target_sdk": "1"
+                }
+            ]
+        }
+    ],
+    "rows": [
+        {
+            "package": "Messaging",
+            "component": "ListMsgs",
+            "domain_data": [
+                {
+                    "domain": "explicit_domain",
+                    "data": [
+                        0, 1, 0, 0, 0, 0
+                    ]
+                },
+                {
+                    "domain": "implicit_domain",
+                    "data": [
+                        0, 0, 0, 0, 0, 0
+                    ]
+                },
+                {
+                    "domain": "permission_granted_domain",
+                    "data": [
+                        0, 0, 1
+                    ]
+                },
+                {
+                    "domain": "permission_usage_domain",
+                    "data": [
+                        0, 0, 1
+                    ]
+                }
+            ]
+        },
+        {
+            "package": "Messaging",
+            "component": "Composer",
+            "domain_data": [
+                {
+                    "domain": "explicit_domain",
+                    "data": [
+                        1, 0, 0, 0, 0, 0
+                    ]
+                },
+                {
+                    "domain": "implicit_domain",
+                    "data": [
+                        0, 0, 1, 0, 0, 0
+                    ]
+                },
+                {
+                    "domain": "permission_granted_domain",
+                    "data": [
+                        0, 1, 0
+                    ]
+                },
+                {
+                    "domain": "permission_usage_domain",
+                    "data": [
+                        0, 0, 0
+                    ]
+                }
+            ]
+        }
+    ]
+};
+
+function renderJsonTable2(table, div_id) {
+    //create table
+    create_table_structure(div_id);
+
+    //create all headers
+    create_all_table_headers2(div_id, table);
+
+    //create rows
+    create_all_table_rows2(div_id, table);
+}
+
+function create_table_structure(div_id) {
+    d3.select("#" + div_id)
+        .append("table")
+        .attr("id", div_id + "_table")
+        .append("thead")
+        .attr("id", div_id + "_thead");
+
+    d3.select("#" + div_id + "_table")
+        .append("tbody")
+        .attr("id", div_id + "_tbody");
+}
+
+function create_all_table_rows2(div_id, table) {
+    var row_data = [];
+    var domain_data = [];
+
+    table.rows.forEach(function(row) {
+        row.domain_data.forEach(function(domain) {
+            domain.data.forEach(function(data) {
+                domain_data.push(data);
+            });
+        });
+        row_data.push(domain_data);
+        domain_data = [];
+    });
+
+    row_data.forEach(function(data) {
+        create_table_rows2(div_id, data);
+    });
+}
+
+function create_table_rows2(div_id, rows) {
+    d3.select("#" + div_id + "_tbody")
+        .append("tr")
+        .selectAll("td")
+        .data(rows)
+        .enter()
+        .append("td")
+        .text( function(d) { return d; })
+}
+
+//accepts an array of a domains and their subdomains
+function create_all_table_headers2(div_id, table) {
+    var sub_headers = [];
+    var domain_names = [];
+    var domain_headers = {};
+    domain_headers.colspan = [];
+
+    var num_columns_added = 0;
+
+    table.domains.forEach(function(domain) {
+        domain_names.push(domain.name);
+        domain_headers.colspan.push(domain.elements.length);
+
+        //create subheader array
+        domain.elements.forEach(function(element) {
+            sub_headers.push(element.name);
+        });
+
+        num_columns_added++;
+    });
+
+    if(num_columns_added == 3) {
+        print_debug(sub_headers);
+        print_debug(domain_names);
+        print_debug(domain_headers);
+    }
+
+    domain_headers.domains = domain_names;
+    create_table_headers2(div_id, domain_headers);
+    create_table_sub_headers2(div_id, sub_headers);
+}
+
+function create_table_sub_headers2(div_id, headers) {
+    d3.select("#" + div_id + "_thead")
+        .append("tr")
+        .selectAll("th")
+        .data(headers)
+        .enter()
+        .append("th")
+        .text( function(d) { return d; });
+}
+
+function create_table_headers2(div_id, headers) {
+    d3.select("#" + div_id + "_thead")
+        .append("tr")
+        .selectAll("th")
+        .data(headers.domains)
+        .enter()
+        .append("th")
+        .attr("colspan", function(d, i) { return headers.colspan[i]; } )
+        .text( function(d) { return d; });
+}
+
+renderJsonTable2(tableJSON2, "example8");
