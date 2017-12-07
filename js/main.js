3dc241a26c4b1a939aeea2ff19020cc90b67b72a
Felix Lee <fchanlee@mail.sfsu.edu>
2017-12-06 23:29:36 -0800

    Swithed out custom classes for data- attributes. These attributes will give elements state that we can use to select the specific elements we want and manipulate them.

--- js/main.js
+++ js/main.js
@@ -402,11 +402,11 @@ let tableJSON = {
                     "domain_data": [
                         {
                             "domain": "explicit",
-                            "data": [0, 1, 0, 0, 0, 0]
+                            "data": [0, 0, 0, 1, 0, 0]
                         },
                         {
                             "domain": "implicit",
-                            "data": [0, 0, 0, 0, 0, 0]
+                            "data": [0, 0, 0, 1, 0, 0]
                         },
                         {
                             "domain": "permission granted",
@@ -429,11 +429,11 @@ let tableJSON = {
                     "domain_data": [
                         {
                             "domain": "explicit",
-                            "data": [1, 0, 0, 0, 0, 0]
+                            "data": [0, 0, 1, 0, 0, 0]
                         },
                         {
                             "domain": "implicit",
-                            "data": [0, 0, 1, 0, 0, 0]
+                            "data": [0, 0, 0, 0, 0, 0]
                         },
                         {
                             "domain": "permission granted",
@@ -474,8 +474,23 @@ $(document).ready( function(){
         $package_cell.attr("data-title", the_package.package);
         $package_cell.attr("data-content", "Some info that we can parse out and include in the JSON later");
 
+        //attach popover info to component cells
+        the_package.components.forEach(function (component) {
+            let css_component_cell = "tr td.data-cpn_" + component.component;
+            let $component_cell = $(css_component_cell);
 
-        the_package.components.forEach( function(component) {
+            $component_cell.attr("data-toggle", "popover");
+            $component_cell.attr("data-container", css_component_cell);
+            $component_cell.attr("data-trigger", "click");
+            $component_cell.attr("data-placement", "auto");
+            $component_cell.attr("data-html", "true");
+            $component_cell.attr("data-template", '<div class=\"popover\" role=\"tooltip\"><div class=\"arrow\"></div><h3 class=\"popover-header\"></h3><div class=\"popover-body\"></div></div>');
+            $component_cell.attr("data-title", component.component);
+            $component_cell.attr("data-content", "<p>Type: " + component.type + "</p>" + "<p>Extra: Some extra info</p>");
+        });
+
+        //attach popover info to attack cells
+        the_package.components.forEach(function (component) {
             let css_component_cell = "tr td.data-cpn_" + component.component;
             let $component_cell = $(css_component_cell);
 
@@ -489,8 +504,6 @@ $(document).ready( function(){
             $component_cell.attr("data-content", "<p>Type: " + component.type + "</p>" + "<p>Extra: Some extra info</p>");
         });
     });
-
-
 
     //initializes all popovers
     $('[data-toggle="popover"]').popover();
@@ -523,25 +536,60 @@ function create_all_table_rows(div_id, table) {
     let rows_data = [];
     let grouped_domain_data = [];
     let domain_classes = [];
+    let permission_classes = [];
 
     //prepare rows data
     table.packages.forEach(function(the_package) {
         the_package.components.forEach(function(component) {
             component.domain_data.forEach(function(domain_datum) {
 
+                //uncomment below to revert to displaying all permission subdomain data
+                //
                 //maps each element in grouped_domain_data with their respective column header
+                // table.domains.forEach(function(domain) {
+                //     if(domain.name === domain_datum.domain) {
+                //         domain.subdomains.forEach(function(subdomain) {
+                //             domain_classes.push(domain.name.toString().replace(/\s/g, "-") +
+                //                 "_" + subdomain.name.toString().replace(/\s/g, "-"));
+                //         });
+                //     }
+                // });
+                //
+                // domain_datum.data.forEach(function(data) {
+                //     grouped_domain_data.push(data);
+                // });
+
+                //maps each element in grouped_domain_data with their respective column headers
                 table.domains.forEach(function(domain) {
                     if(domain.name === domain_datum.domain) {
-                        domain.subdomains.forEach(function(subdomain) {
-                            domain_classes.push(domain.name.toString().replace(/\s/g, "-") +
-                                "_" + subdomain.name.toString().replace(/\s/g, "-"));
-                        });
+                        //skip this step for permission domains since we will not list all of their subdomains
+                        if (!domain_datum.domain.toString().match(/^permission*/)) {
+                            domain.subdomains.forEach(function (subdomain) {
+                                domain_classes.push(domain.name.toString().replace(/\s/g, "-") +
+                                    "_dsmidx-" + subdomain.name.toString().replace(/\s/g, "-"));
+                            });
+                        }
+                        else {
+                            permission_classes.push(domain.name.toString().replace(/\s/g, "-"));
+                        }
                     }
                 });
 
-                domain_datum.data.forEach(function(data) {
-                    grouped_domain_data.push(data);
-                });
+                //group the row data for each non-permission domain
+                if(!domain_datum.domain.toString().match(/^permission*/)) {
+                    domain_datum.data.forEach(function (data) {
+                        grouped_domain_data.push(data);
+                    });
+                }
+                else {
+                    //empty cell for the permission domains
+                    grouped_domain_data.push("Click Me!");
+                }
+            });
+
+            //add in the permission classes to the array of domain classes
+            permission_classes.forEach(function(perm_class) {
+                domain_classes.push(perm_class);
             });
 
             let data = {
@@ -567,7 +615,6 @@ function create_all_table_rows(div_id, table) {
             create_table_rows(div_id, data, data.components_count);
         }
         else {
-            //1 rowspan because we already set the rowspan
             create_table_rows(div_id, data, false);
         }
 
@@ -582,21 +629,26 @@ function create_table_rows(div_id, rows, rowspan) {
 
     let row = d3.select("#" + div_id + "_tbody")
         .append("tr")
-        .classed(class_package, true)
-        .classed(class_component, true)
-        .classed(class_dsm_idx, true);
+        .attr("data-pkg", rows.package)
+        .attr("data-cpn", rows.component)
+        .attr("data-dsmidx", rows.component_dsm_idx)
+        // .classed(class_package, true)
+        // .classed(class_component, true)
+        // .classed(class_dsm_idx, true);
 
     //create package headers. If rowspan is specified, add it, otherwise, skip creating it
     if(rowspan !== false) {
         row.append("td")
             .attr("rowspan", rowspan)
-            .classed(class_package, true)
+            .attr("data-pkg", rows.package)
+            // .classed(class_package, true)
             .text(rows.package);
     }
 
     //create component headers
     row.append("td")
-        .classed(class_component, true)
+        .attr("data-cpn", rows.component)
+        // .classed(class_component, true)
         .text(rows.component);
 
     //create ID header
@@ -604,14 +656,19 @@ function create_table_rows(div_id, rows, rowspan) {
         .text(rows.component_dsm_idx);
 
     //populate domain data
-    d3.select("tr." + class_package + "." + class_component)
+    // d3.select("tr." + class_package + "." + class_component)
+    d3.select("tr[data-pkg=" + rows.package + "][data-cpn=" + rows.component + "]")
         .selectAll("td.g") //g to create a new grouping? and prevent rebinding data to the first two td headers
         .data(rows.data)
         .enter()
         .append("td")
-        .attr("class", function(d, i) {
-            return "data-col_" + rows.domain_classes[i];
+        .attr("data-row", rows.package + "_" + rows.component + "_dsmidx-" + rows.component_dsm_idx)
+        .attr("data-col", function(d, i) {
+            return rows.domain_classes[i];
         })
+        // .attr("class", function(d, i) {
+        //     return "data-col_" + rows.domain_classes[i];
+        // })
         // .classed("data-row_" + rows.component_dsm_idx, true)
         .text( function(d) { return d; })
 }
@@ -626,12 +683,31 @@ function create_all_table_headers(div_id, table) {
     //prepare all domain header arrays
     table.domains.forEach(function(domain) {
         domain_headers.domains.push(domain.name);
-        domain_headers.colspan.push(domain.subdomains.length);
 
-        //create subdomains array
-        domain.subdomains.forEach(function(subdomain) {
-            domain_headers.subdomains.push(subdomain.name);
-        });
+        //uncomment below to revert to displaying all subdomain permissions
+        // domain_headers.domains.push(domain.name);
+        // domain_headers.colspan.push(domain.subdomains.length);
+        //
+        // //create subdomains array
+        // domain.subdomains.forEach(function (subdomain) {
+        //     domain_headers.subdomains.push(subdomain.name);
+        // });
+
+        //create subdomains if not a permissions domain
+        if(!domain.name.toString().match(/^permission*/)) {
+            domain_headers.colspan.push(domain.subdomains.length);
+
+            //create subdomains array
+            domain.subdomains.forEach(function (subdomain) {
+                domain_headers.subdomains.push(subdomain.name);
+            });
+        }
+        else {
+            //no subdomains
+            domain_headers.subdomains.push(" ");
+            //permission domains should have colspan of 1
+            domain_headers.colspan.push(1);
+        }
     });
 
     create_domain_headers(div_id, domain_headers);
@@ -690,9 +766,9 @@ var analysisJson = [
         "attack_type": "privilege escalation",
         "attack_info": {
             "malicious_dsmidx": 2,
-            "malicious_component": "LevelUp",
-            "vulnerable_dsmidx": 1,
-            "vulnerable_component": "Sender",
+            "malicious_component": "ListMsgs",
+            "vulnerable_dsmidx": 3,
+            "vulnerable_component": "Composer",
             "resource_dsmidx": 6,
             "resource": "SMS"
         }
@@ -702,7 +778,7 @@ var analysisJson = [
         "attack_info": {
             "malicious_dsmidx": 3,
             "malicious_component": "LevelUp",
-            "vulnerable_dsmidx": 0,
+            "vulnerable_dsmidx": 2,
             "vulnerable_component": "Sender",
             "pot_dsmidx": 6,
             "pot_component": "SMS"
@@ -729,34 +805,32 @@ function render_analysis() {
 
 function render_attack(attack) {
     let coords = {};
-
     coords.row = attack.attack_info.malicious_dsmidx;
     coords.column = attack.attack_info.vulnerable_dsmidx;
 
+    // let attack_type = attack.attack_type.toString().replace(/\s/g, "-");
     let attack_type = attack.attack_type;
-    let attack_class = "";
-
-    if(attack_type === "privilege_escalation") {
-        attack_class = "data-attack_privilege-escalation";
-    }
-    else if(attack_type === "intent_spoofing") {
-        attack_class = "data-attack_intent-spoofing";
-    }
-    else if(attack_class === "unauthorized_intent-receipt") {
-        attack_class = "data-attack_unauthorized_intent-receipt";
-    }
 
     //add checks that the table cell is a 1 other wise don't render the attack
-    let table_cell_implicit = $("tr.data-dsmidx_" + coords.row + " td.data-col_implicit_" + coords.column);
-    let table_cell_explicit = $("tr.data-dsmidx_" + coords.row + " td.data-col_explicit_" + coords.column);
+    // let table_cell_implicit = $("tr[data-dsmidx=" + coords.row + "] td[data-col*=dsmidx-" + coords.column);
+    // let table_cell_explicit = $("tr.data-dsmidx_" + coords.row + " td.data-col_explicit_" + coords.column);
 
-    if(table_cell_implicit.html() === "1") {
-        table_cell_implicit.addClass(attack_class);
-    }
+    let $table_cells = $("tr[data-dsmidx='" + coords.row + "'] td[data-col*='dsmidx-" + coords.column + "']");
+    // tr[data-dsmidx="2"] td[data-col*="dsmidx-3"]
+    $table_cells.each(function() {
+        if($(this).html() === "1") {
+            $(this).attr("data-attack", attack_type);
+        }
+    });
 
-    if(table_cell_explicit.html() === "1") {
-        table_cell_explicit.addClass(attack_class);
-    }
+
+    // if(table_cell_implicit.html() === "1") {
+    //     table_cell_implicit.addClass(attack_class);
+    // }
+    //
+    // if(table_cell_explicit.html() === "1") {
+    //     table_cell_explicit.addClass(attack_class);
+    // }
 
     console.log(coords.row);
 }
