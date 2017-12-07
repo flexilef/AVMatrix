f9454200104070e0697b320553f26141c23c4525
Felix Lee <fchanlee@mail.sfsu.edu>
2017-12-07 00:08:14 -0800

    Added attack info popovers. Removed subdomains from permission domains.

--- js/main.js
+++ js/main.js
@@ -454,6 +454,42 @@ let tableJSON = {
     ]
 };
 
+let attackJson = [
+    {
+        "attack_type": "privilege escalation",
+        "attack_info": {
+            "malicious_dsmidx": 2,
+            "malicious_component": "ListMsgs",
+            "vulnerable_dsmidx": 3,
+            "vulnerable_component": "Composer",
+            "resource_dsmidx": 6,
+            "resource": "SMS"
+        }
+    },
+    {
+        "attack_type": "intent spoofing",
+        "attack_info": {
+            "malicious_dsmidx": 3,
+            "malicious_component": "LevelUp",
+            "vulnerable_dsmidx": 2,
+            "vulnerable_component": "Sender",
+            "pot_dsmidx": 6,
+            "pot_component": "SMS"
+        }
+    },
+    {
+        "attack_type": "unauthorized intent receipt",
+        "attack_info": {
+            "malicious_dsmidx": 0,
+            "malicious_component": "LevelUp",
+            "vulnerable_dsmidx": 3,
+            "vulnerable_component": "Sender",
+            "pot_dsmidx": 6,
+            "pot_component": "SMS"
+        }
+    }
+];
+
 $(document).ready( function(){
     renderJsonTable(tableJSON, "example8");
     render_analysis();
@@ -461,7 +497,7 @@ $(document).ready( function(){
     //attach popover info to table cells
     tableJSON.packages.forEach( function(the_package) {
         //attach popover info to package cells
-        let css_package_cell = "tr td.data-pkg_" + the_package.package;
+        let css_package_cell = "tr td[data-pkg='" + the_package.package + "']";
         let $package_cell = $(css_package_cell);
 
         $package_cell.attr("data-toggle", "popover");
@@ -476,7 +512,7 @@ $(document).ready( function(){
 
         //attach popover info to component cells
         the_package.components.forEach(function (component) {
-            let css_component_cell = "tr td.data-cpn_" + component.component;
+            let css_component_cell = "tr td[data-cpn='" + component.component + "'";
             let $component_cell = $(css_component_cell);
 
             $component_cell.attr("data-toggle", "popover");
@@ -488,21 +524,44 @@ $(document).ready( function(){
             $component_cell.attr("data-title", component.component);
             $component_cell.attr("data-content", "<p>Type: " + component.type + "</p>" + "<p>Extra: Some extra info</p>");
         });
+    });
 
-        //attach popover info to attack cells
-        the_package.components.forEach(function (component) {
-            let css_component_cell = "tr td.data-cpn_" + component.component;
-            let $component_cell = $(css_component_cell);
+    //attach popover info to attack cells
+    let css_attack_cells = "tr td[data-attack='true'";
+    let $attack_cells = $(css_attack_cells);
 
-            $component_cell.attr("data-toggle", "popover");
-            $component_cell.attr("data-container", css_component_cell);
-            $component_cell.attr("data-trigger", "click");
-            $component_cell.attr("data-placement", "auto");
-            $component_cell.attr("data-html", "true");
-            $component_cell.attr("data-template", '<div class=\"popover\" role=\"tooltip\"><div class=\"arrow\"></div><h3 class=\"popover-header\"></h3><div class=\"popover-body\"></div></div>');
-            $component_cell.attr("data-title", component.component);
-            $component_cell.attr("data-content", "<p>Type: " + component.type + "</p>" + "<p>Extra: Some extra info</p>");
+    $attack_cells.each(function() {
+        let attack_info = {};
+        let attack_type = $(this).attr("data-attacktype");
+
+        //get the last element split by _ and then get the last value split by -, which will give you the number without "dsmidx"
+        let attacker_dsmidx = $(this).attr("data-row").toString().split("_").pop().split("-").pop();
+        let victim_dsmidx = $(this).attr("data-col").toString().split("_").pop().split("-").pop();
+
+        //get attack info
+        attackJson.forEach(function(attack) {
+            if(attack.attack_type === attack_type) {
+                // console.log(victim_dsmidx);
+
+                if(attack.attack_info.malicious_dsmidx.toString() === attacker_dsmidx &&
+                    attack.attack_info.vulnerable_dsmidx.toString() === victim_dsmidx) {
+                    attack_info.attacker = attack.attack_info.malicious_component;
+                    attack_info.victim = attack.attack_info.vulnerable_component;
+
+                    console.log(attack_info.attacker);
+                }
+            }
         });
+
+        // console.log(attack_type);
+
+        $(this).attr("data-toggle", "popover");
+        $(this).attr("data-trigger", "click");
+        $(this).attr("data-placement", "auto");
+        $(this).attr("data-html", "true");
+        $(this).attr("data-template", '<div class=\"popover\" role=\"tooltip\"><div class=\"arrow\"></div><h3 class=\"popover-header\"></h3><div class=\"popover-body\"></div></div>');
+        $(this).attr("data-title", attack_type);
+        $(this).attr("data-content", "<p>Attacker: " + attack_info.attacker + "</p>" + "<p>Victim: " + attack_info.victim + "</p>");
     });
 
     //initializes all popovers
@@ -623,32 +682,23 @@ function create_all_table_rows(div_id, table) {
 }
 
 function create_table_rows(div_id, rows, rowspan) {
-    const class_package =  "data-pkg_" + rows.package;
-    const class_component =  "data-cpn_" + rows.component;
-    const class_dsm_idx = "data-dsmidx_" + rows.component_dsm_idx;
-
     let row = d3.select("#" + div_id + "_tbody")
         .append("tr")
         .attr("data-pkg", rows.package)
         .attr("data-cpn", rows.component)
         .attr("data-dsmidx", rows.component_dsm_idx)
-        // .classed(class_package, true)
-        // .classed(class_component, true)
-        // .classed(class_dsm_idx, true);
 
     //create package headers. If rowspan is specified, add it, otherwise, skip creating it
     if(rowspan !== false) {
         row.append("td")
             .attr("rowspan", rowspan)
             .attr("data-pkg", rows.package)
-            // .classed(class_package, true)
             .text(rows.package);
     }
 
     //create component headers
     row.append("td")
         .attr("data-cpn", rows.component)
-        // .classed(class_component, true)
         .text(rows.component);
 
     //create ID header
@@ -656,7 +706,6 @@ function create_table_rows(div_id, rows, rowspan) {
         .text(rows.component_dsm_idx);
 
     //populate domain data
-    // d3.select("tr." + class_package + "." + class_component)
     d3.select("tr[data-pkg=" + rows.package + "][data-cpn=" + rows.component + "]")
         .selectAll("td.g") //g to create a new grouping? and prevent rebinding data to the first two td headers
         .data(rows.data)
@@ -666,10 +715,6 @@ function create_table_rows(div_id, rows, rowspan) {
         .attr("data-col", function(d, i) {
             return rows.domain_classes[i];
         })
-        // .attr("class", function(d, i) {
-        //     return "data-col_" + rows.domain_classes[i];
-        // })
-        // .classed("data-row_" + rows.component_dsm_idx, true)
         .text( function(d) { return d; })
 }
 
@@ -683,15 +728,6 @@ function create_all_table_headers(div_id, table) {
     //prepare all domain header arrays
     table.domains.forEach(function(domain) {
         domain_headers.domains.push(domain.name);
-
-        //uncomment below to revert to displaying all subdomain permissions
-        // domain_headers.domains.push(domain.name);
-        // domain_headers.colspan.push(domain.subdomains.length);
-        //
-        // //create subdomains array
-        // domain.subdomains.forEach(function (subdomain) {
-        //     domain_headers.subdomains.push(subdomain.name);
-        // });
 
         //create subdomains if not a permissions domain
         if(!domain.name.toString().match(/^permission*/)) {
@@ -761,44 +797,8 @@ function create_domain_headers(div_id, headers) {
 Example 10 - rendering analysis results (testing is json structure is good)
  */
 
-var analysisJson = [
-    {
-        "attack_type": "privilege escalation",
-        "attack_info": {
-            "malicious_dsmidx": 2,
-            "malicious_component": "ListMsgs",
-            "vulnerable_dsmidx": 3,
-            "vulnerable_component": "Composer",
-            "resource_dsmidx": 6,
-            "resource": "SMS"
-        }
-    },
-    {
-        "attack_type": "intent spoofing",
-        "attack_info": {
-            "malicious_dsmidx": 3,
-            "malicious_component": "LevelUp",
-            "vulnerable_dsmidx": 2,
-            "vulnerable_component": "Sender",
-            "pot_dsmidx": 6,
-            "pot_component": "SMS"
-        }
-    },
-    {
-        "attack_type": "unauthorized intent receipt",
-        "attack_info": {
-            "malicious_dsmidx": 0,
-            "malicious_component": "LevelUp",
-            "vulnerable_dsmidx": 3,
-            "vulnerable_component": "Sender",
-            "pot_dsmidx": 6,
-            "pot_component": "SMS"
-        }
-    }
-];
-
 function render_analysis() {
-    analysisJson.forEach(function(attack) {
+    attackJson.forEach(function(attack) {
         render_attack(attack);
     });
 }
@@ -808,29 +808,15 @@ function render_attack(attack) {
     coords.row = attack.attack_info.malicious_dsmidx;
     coords.column = attack.attack_info.vulnerable_dsmidx;
 
-    // let attack_type = attack.attack_type.toString().replace(/\s/g, "-");
     let attack_type = attack.attack_type;
 
-    //add checks that the table cell is a 1 other wise don't render the attack
-    // let table_cell_implicit = $("tr[data-dsmidx=" + coords.row + "] td[data-col*=dsmidx-" + coords.column);
-    // let table_cell_explicit = $("tr.data-dsmidx_" + coords.row + " td.data-col_explicit_" + coords.column);
-
     let $table_cells = $("tr[data-dsmidx='" + coords.row + "'] td[data-col*='dsmidx-" + coords.column + "']");
-    // tr[data-dsmidx="2"] td[data-col*="dsmidx-3"]
     $table_cells.each(function() {
         if($(this).html() === "1") {
-            $(this).attr("data-attack", attack_type);
+            $(this).attr("data-attacktype", attack_type);
+            $(this).attr("data-attack", true);
         }
     });
-
-
-    // if(table_cell_implicit.html() === "1") {
-    //     table_cell_implicit.addClass(attack_class);
-    // }
-    //
-    // if(table_cell_explicit.html() === "1") {
-    //     table_cell_explicit.addClass(attack_class);
-    // }
 
     console.log(coords.row);
 }
